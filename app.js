af50a6d13d051bdcc7becd0b8ce4a288a22b3642
Adnan Jusic <adnanjusic98@gmail.com>
2022-02-02 19:00:05 +0100

    Some simple DOM stuff

---
+++ app.js
@@ -0,0 +1,23 @@
+let container = document.querySelector('#container');
+
+let par = document.createElement('p');
+container.appendChild(par);
+par.innerText = 'Hey I\'m red!';
+par.style.color = 'red';
+
+let h3 = document.createElement('h3');
+container.appendChild(h3);
+h3.style.color = 'blue';
+h3.innerText = 'I\'m a blue h3';
+
+let div = document.createElement('div');
+container.appendChild(div);
+div.style.cssText = 'border: black solid 1px; background-color: pink';
+
+let h1 = document.createElement('h1');
+div.appendChild(h1);
+h1.innerText = 'Im in a div';
+
+let par2 = document.createElement('p');
+div.appendChild(par2);
+par2.innerText = 'Me too';
